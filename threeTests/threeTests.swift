1290965c074747428f7ec6ffc50c9c55aca4468b
greggal <leninson@gmail.com>
2025-07-29 11:26:02 +0300

    Initial Commit

---
+++ threeTests/threeTests.swift
@@ -0,0 +1,17 @@
+//
+//  threeTests.swift
+//  threeTests
+//
+//  Created by Grigoriy Galperin on 29.07.2025.
+//
+
+import Testing
+@testable import three
+
+struct threeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
